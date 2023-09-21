2efdb20c9b206dfc59b4d557ea1ae34c67be1dfb
FelixTEK <zuhong2005@gmail.com>
2023-09-21 08:03:52 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#FELIXTEK2023
+def primeornot(): #Function to find prime number
+    
+    number =  int(input("Insert desired number>"))
+    for i in range (2,number - 1):
+        if number % i == 0:
+            
+        #i = i + 1
+
+
+if __name__ == "__main__": #Main function
+    print()
