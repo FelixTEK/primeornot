80d14efa6457eef5548f486f992d5f69b99059d3
FelixTEK <zuhong2005@gmail.com>
2023-09-27 23:21:25 +0700

    Update main.py
    Program operational. Might throw errors when values less than 3 is inserted.

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ def primeornot(): #Function to find prime number
     number =  int(input("Insert desired number>"))
     for i in range (2,number - 1):
         if number % i == 0:
+            print("This number isn't prime.")
+            break
+        else:
+            i = i + 1
+    if i == number - 1:
+        print("This number is prime.")        
             
-        #i = i + 1
-
-
 if __name__ == "__main__": #Main function
-    print()
+    primeornot()
